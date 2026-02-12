f190f140331bcf53ca9d5f8ab9cb069a6973bce4
PsyMarth <Dean.Laganiere@gmail.com>
2026-02-12 15:09:06 -0800

    Add non-zero egg requirement test case.

--- worlds/rabi_ribi/test/TestGoal.py
+++ worlds/rabi_ribi/test/TestGoal.py
@@ -16,3 +16,19 @@ class GoalTestEggRounding(RabiRibiTestBase):
             item = self.get_item_by_name(ItemName.easter_egg)
             self.collect(item)
         self.assertBeatable(True)
+
+class GoalTestEggNonZero(RabiRibiTestBase):
+    options = {
+        "max_number_of_easter_eggs": 10,
+        "percentage_of_easter_eggs": 1
+    }
+
+    def test_egg_non_zero(self) -> None:
+        """
+        Ensure that at least one egg is always required to beat the game.
+        """
+        self.assertBeatable(False)
+        for _ in range(1):
+            item = self.get_item_by_name(ItemName.easter_egg)
+            self.collect(item)
+        self.assertBeatable(True)
